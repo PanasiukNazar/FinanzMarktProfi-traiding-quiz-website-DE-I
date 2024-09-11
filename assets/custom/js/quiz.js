bd6860d7be50c5af038ee692c0bdb44308ba0d12
NazarPanasiuk <nazar.panasiuk.ua@gmail.com>
2024-09-11 23:35:21 +0300

    WIP

---
+++ assets/custom/js/quiz.js
@@ -0,0 +1,194 @@
+const QUESTIONS = [
+    {
+        label: 'Wie alt sind Sie?',
+        answers: ['25-35', '35-45', '45-55'],
+    },
+    {
+        label: 'Hatten Sie Erfahrung im Trading?',
+        answers: ['Ja', 'Nein'],
+    },
+    {
+        label: 'Was genau interessiert Sie?',
+        answers: ['Ausbildung', 'Einkommen'],
+    },
+    {
+        label: 'Haben Sie zuvor automatische Handelsdienste genutzt?',
+        answers: ['Ja', 'Nein'],
+    },
+];
+
+const $container = document.getElementById('container');
+
+const startStep = {
+    render: () => {
+        $container.innerHTML = `
+        <div class="container quiz-wrapper">
+            <div class="row quiz-content">
+                <div class="col-lg-6 col-md-6 col-lg-6">
+                    <img class="quiz-img" src="assets/custom/images/quiz.jpg">
+                </div>
+                <div class="col-lg-6 col-md-6 col-lg-6">
+                    <h2 class="title">Trading-Ausbildung auf Ihrem Handy</h2>
+                    <h2 class="title">Team "Trader Bo"t: 45 Minuten kostenlose Beratung</h2>
+                    <h3>30-minütige Beratung durch einen Buchhalter</h3>
+                    <button class="btn btn-primary w-100 py-3 first-button" data-action="startQuiz">Beginnen</button>
+                </div>
+            </div>
+        </div>
+      `;
+    },
+    onClick: (el) => {
+        if (el.getAttribute('data-action') === 'startQuiz') {
+            quiz.nextStep(questionsStep);
+        }
+    },
+};
+
+const questionsStep = {
+    questionIndex: 0,
+    answers: {},
+    render: () => {
+        const question = QUESTIONS[questionsStep.questionIndex];
+
+        $container.innerHTML = `
+        <div class="container quiz-wrapper">
+
+            <div class="row quiz-content text-center">
+                <div class="question-wrapper">
+                    <div class="row justify-content-center mt-4" style="width: 100%;">
+                        <div class="progress col-md-6" style="padding-left: 0 !important; padding-right: 0 !important;">
+                            <div class="progress-bar" style="width: ${questionsStep.getProgress()}%"></div>
+                        </div>
+                    </div>
+
+                    <h3 class="question mt-4">${question.label}</h3>
+                </div>
+
+                <div class="row answers">
+                    ${question.answers
+                        .map(
+                            (answer, index) =>
+                                `
+                                <button class="answer col-md-10 border rounded" data-action="selectAnswer" data-answer-index="${index}">
+                                    ${answer}
+                                </button>
+                            `,
+                        )
+                        .join('')}
+                </div>
+            </div>
+        </div>
+      `;
+    },
+    getProgress: () =>
+        Math.floor((questionsStep.questionIndex / QUESTIONS.length) * 100),
+    onClick: (el) => {
+        switch (el.getAttribute('data-action')) {
+            case 'goToNextQuestion':
+                return questionsStep.goToNextQuestion();
+            case 'goToPreviousQuestion':
+                return questionsStep.goToPreviousQuestion();
+            case 'selectAnswer':
+                return questionsStep.selectAnswer(
+                    parseInt(el.getAttribute('data-answer-index'), 10),
+                );
+        }
+    },
+    goToPreviousQuestion: () => {
+        questionsStep.questionIndex -= 1;
+        questionsStep.render();
+    },
+    selectAnswer: (answerIndex) => {
+        const question = QUESTIONS[questionsStep.questionIndex];
+        const selectedAnswer = question.answers[answerIndex];
+
+        questionsStep.answers = {
+            ...questionsStep.answers,
+            [question.label]: selectedAnswer,
+        };
+
+        if (questionsStep.isFinalQuestion()) {
+            questionsStep.completeStep();
+        } else {
+            questionsStep.goToNextQuestion();
+        }
+    },
+    isFinalQuestion: () => questionsStep.questionIndex === QUESTIONS.length - 1,
+    goToNextQuestion: () => {
+        questionsStep.questionIndex += 1;
+        questionsStep.render();
+    },
+    completeStep: () => {
+        quiz.setAnswers(questionsStep.answers);
+        quiz.nextStep(finalStep);
+    },
+};
+
+const finalStep = {
+    render: () => {
+        $container.innerHTML = `
+        <div class="container quiz-wrapper">
+            <div class="row quiz-content">
+                <div class="col-lg-6 col-md-6 col-sm-12 form-block">
+                    <h2 class="title">Form der Kommunikation</h2>
+                    <h3 class="mb-4">Bitte füllen Sie das Feedback-Formular aus</h3>
+                    <form>
+                        <input class="form-control" name="name" type="name" placeholder="Name" required>
+                        <input class="form-control" name="Surname" type="name" placeholder="Nachname" required>
+                        <input class="form-control" name="email" type="email" placeholder="E-Mail" required>
+                        <input class="form-control" name="phone" type="phone" placeholder="Telefon" required>
+
+                        
+                        ${Object.keys(quiz.answers)
+                            .map(
+                                (question) =>
+                                    `<input name="${question}" value="${quiz.answers[question]}" hidden>`,
+                            )
+                            .join('')}
+                
+                        <button data-action="submitAnswers" class="btn btn-primary w-100 py-3 first-button">Senden Sie</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+      `;
+    },
+    onClick: (el) => {
+        const newPath = 'thanks.html';
+        if (el.getAttribute('data-action') === 'submitAnswers') {
+            localStorage.setItem('quizDone', true);
+            document.getElementById('main-page').classList.remove('hide');
+            document.getElementById('quiz-page').classList.add('hide');
+            window.location.href = newPath;
+        }
+    },
+};
+
+const quiz = {
+    activeStep: startStep,
+    answers: {},
+    clear: () => ($container.innerHTML = ''),
+    init: () => {
+        $container.addEventListener('click', (event) =>
+            quiz.activeStep.onClick(event.target),
+        );
+        $container.addEventListener('submit', (event) =>
+            event.preventDefault(),
+        );
+    },
+    render: () => {
+        quiz.clear();
+        quiz.activeStep.render();
+    },
+    nextStep: (step) => {
+        quiz.activeStep = step;
+        quiz.render();
+    },
+    setAnswers: (answers) => (quiz.answers = answers),
+};
+
+if (!localStorage.getItem('quizDone')) {
+    document.getElementById('main-page').classList.add('hide');
+    quiz.init();
+    quiz.render();
+}
